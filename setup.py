96f9adc44e8be160cca7d0e4ca4f3e5f9b6f18f2
Daniel Smilkov <smilkov@smilkov-macbookpro1.roam.corp.google.com>
2017-07-11 18:01:38 -0400

    Fix invalid tags in setup.py

--- setup.py
+++ setup.py
@@ -43,14 +43,10 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Neural networks',
-
-        # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: Apache 2.0',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
