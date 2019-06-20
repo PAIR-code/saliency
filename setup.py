49e4b7f62a8480f24a90d2df76ac48526bdbc73d
Sherry Yang <sherryy@google.com>
2019-06-20 11:07:47 -0700

    Use markdown as long description content type to properly render description on Pypi.

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
 
     description='Saliency methods for TensorFlow',
     long_description=long_description,
+    long_description_content_type='text/markdown',
 
     # The project's main homepage.
     url='https://github.com/pair-code/saliency',
