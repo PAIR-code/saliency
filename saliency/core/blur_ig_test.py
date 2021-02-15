1ec459548fcb1c5fe99814f7cbf7c55887217eb4
Ben Wedin <ben.wedin@gmail.com>
2021-02-15 15:07:02 -0500

    Clean up comments/documentation in tf1 subpackage

--- saliency/core/blur_ig_test.py
+++ saliency/core/blur_ig_test.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Google Inc. All Rights Reserved.
+# Copyright 2021 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tests completeness axiom, batching, and error handling for blur_ig.
-"""
+"""Tests completeness axiom, batching, and error handling for blur_ig."""
 import unittest
 
 from . import blur_ig
@@ -23,12 +22,12 @@ OUTPUT_LAYER_GRADIENTS = blur_ig.OUTPUT_LAYER_GRADIENTS
 
 
 class BlurIgTest(unittest.TestCase):
-  """To run: "python -m saliency.blur_ig_test" top-level saliency directory."""
+  """To run: "python -m saliency.core.blur_ig_test" top-level saliency directory."""
 
   def setUp(self):
     super().setUp()
     self.max_sigma = 10
-    # All black except 1 white pixel at the center.
+    # All black except 2 pixels near the center.
     self.x_input_val = np.array([
         [0.0, 0.0, 0.0, 0.0, 0.0],
         [0.0, 0.7, 0.0, 0.0, 0.0],
@@ -46,11 +45,16 @@ class BlurIgTest(unittest.TestCase):
                                             sigma=self.max_sigma)
     y_baseline_val = np.sin(self.x_baseline_val)
 
-    # Test if completeness axiom is satisfied.
     # The expected BlurIG value is equal to the difference between
-    # the `y` value at the input and the `y` value at the baseline.
+    # the `y` value at the input and the `y` value at the baseline. Because each
+    # value is independent, we can calculate the expected blur_ig value of each.
+    #
+    # Expected: [[-0, -0, -0, -0, -0],
+    #            [-0, 0.641, -0, -0, -0],
+    #            [-0, -0, 0.838, -0, -0],
+    #            [-0, -0, -0, -0, -0],
+    #            [-0, -0, -0, -0, -0]
     self.expected_val = y_input_val - y_baseline_val
-    self.expected_argmax = 12  # middle of the 5x5 matrix
     self.blur_ig_instance = blur_ig.BlurIG()
 
   def create_call_model_function(self):
@@ -62,6 +66,7 @@ class BlurIgTest(unittest.TestCase):
     return call_model
 
   def create_bad_call_model_function(self):
+    # Bad call model function, gradients do not match shape of input.
     def call_model(x_value_batch, call_model_args={}, expected_keys=None):
       call_model.num_calls += 1
       return {OUTPUT_LAYER_GRADIENTS: np.cos(x_value_batch)[0]}
@@ -70,6 +75,7 @@ class BlurIgTest(unittest.TestCase):
     return call_model
 
   def testBlurIGGetMask(self):
+    """Tests that BlurIG steps are created and aggregated correctly."""
     x_steps = 4000
     # Create a call_model_function using sess and tensors.
     call_model_function = self.create_call_model_function()
@@ -82,13 +88,12 @@ class BlurIgTest(unittest.TestCase):
     # Because the baseline is blurred, all zero values should still have some 
     # attribution (introduced noise).
     self.assertEqual(np.count_nonzero(mask), mask.size)
-    # Verify the result for completeness..
-    # Expected (for max_sigma=10): 1.41964...
-    # mask.sum (for max_sigma=10): 1.41470...
+    # Verify the result (for accuracy and therefore completeness).
     np.testing.assert_almost_equal(mask, self.expected_val, decimal=2)
     self.assertEqual(call_model_function.num_calls, x_steps)
 
   def testBlurIGGetMaskBatched(self):
+    """Tests that multiple BlurIG batches are created and aggregated correctly."""
     x_steps = 2001
     batch_size = 100
     expected_calls = 21  #  batch size is 100, ceil(2001/100)=21
@@ -107,13 +112,12 @@ class BlurIgTest(unittest.TestCase):
     # Because the baseline is blurred, all zero values should still have some 
     # attribution (introduced noise).
     self.assertEqual(np.count_nonzero(mask), mask.size)
-    # Verify the result for completeness..
-    # Expected (for max_sigma=10): 1.72109
-    # mask.sum (for max_sigma=10): 1.694904...
+    # Verify the result (for accuracy and therefore completeness).
     np.testing.assert_almost_equal(mask, self.expected_val, decimal=2)
     self.assertEqual(call_model_function.num_calls, expected_calls)
 
   def testBlurIGGetMaskSingleBatch(self):
+    """Tests that a single BlurIG batch is created and aggregated correctly."""
     x_steps = 499
     batch_size = 500
     expected_calls = 1  # batch size is 500, ceil(499/500)=1
@@ -132,13 +136,12 @@ class BlurIgTest(unittest.TestCase):
     # Because the baseline is blurred, all zero values should still have some 
     # attribution (introduced noise).
     self.assertEqual(np.count_nonzero(mask), mask.size)
-    # Verify the result for completeness..
-    # Expected (for max_sigma=10): 1.72109
-    # mask.sum (for max_sigma=10): 1.694904...
+    # Verify the result (for accuracy and therefore completeness).
     np.testing.assert_almost_equal(mask, self.expected_val, decimal=2)
     self.assertEqual(call_model_function.num_calls, expected_calls)
 
   def testBlurIGCallModelArgs(self):
+    """Tests that call_model_function receives correct inputs."""
     x_steps = 50
     expected_keys = [OUTPUT_LAYER_GRADIENTS]
     call_model_args = {'foo': 'bar'}
@@ -166,14 +169,14 @@ class BlurIgTest(unittest.TestCase):
           msg='function was called with incorrect expected_keys.')
 
   def testBlurIGGetMaskError(self):
+    """Tests that BlurIG errors with incorrect model outputs."""
     x_steps = 2001
-    # Create a call_model_function using sess and tensors.
     call_model_function = self.create_bad_call_model_function()
     expected_error = blur_ig.SHAPE_ERROR_MESSAGE.format(
         '\\(100, 5, 5, 1\\)','\\(5, 5, 1\\)')
 
+    # Expect error because shape of gradients returned don't match.
     with self.assertRaisesRegex(ValueError, expected_error):
-      # Calculate the Blur IG attribution of the input.
       self.blur_ig_instance.GetMask(
           x_value=self.x_input_val,
           call_model_function=call_model_function,
