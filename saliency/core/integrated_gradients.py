fa98a96b0073a31e94e43514d692d00b02398c48
Ben Wedin <ben.wedin@gmail.com>
2021-03-03 13:34:29 -0500

    Add edited version of core files

--- saliency/core/integrated_gradients.py
+++ saliency/core/integrated_gradients.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
+# Copyright 2021 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,24 +12,46 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Utilities to compute an IntegratedGradients SaliencyMask."""
+"""Utilities to compute saliency using the Integrated Gradients method."""
 
+from .base import CoreSaliency
+from .base import INPUT_OUTPUT_GRADIENTS
 import numpy as np
-from .base import GradientSaliency
 
-class IntegratedGradients(GradientSaliency):
-  """A SaliencyMask class that implements the integrated gradients method.
+
+class IntegratedGradients(CoreSaliency):
+  """A CoreSaliency class that implements the integrated gradients method.
 
   https://arxiv.org/abs/1703.01365
   """
 
-  def GetMask(self, x_value, feed_dict={}, x_baseline=None, x_steps=25):
-    """Returns a integrated gradients mask.
+  def GetMask(self, x_value, call_model_function, call_model_args=None,
+              x_baseline=None, x_steps=25, batch_size=1):
+    """Returns an integrated gradients mask.
 
     Args:
-      x_value: input ndarray.
+      x_value: Input ndarray.
+      call_model_function: A function that interfaces with a model to return
+        specific data in a dictionary when given an input and other arguments.
+        Expected function signature:
+        - call_model_function(x_value_batch,
+                              call_model_args=None,
+                              expected_keys=None):
+          x_value_batch - Input for the model, given as a batch (i.e. dimension
+            0 is the batch dimension, dimensions 1 through n represent a single
+            input).
+          call_model_args - Other arguments used to call and run the model.
+          expected_keys - List of keys that are expected in the output. For this
+            method (Integrated Gradients), the expected keys are
+            INPUT_OUTPUT_GRADIENTS - Gradients of the output being
+              explained (the logit/softmax value) with respect to the input.
+              Shape should be the same shape as x_value_batch.
+      call_model_args: The arguments that will be passed to the call model
+        function, for every call of the model.
       x_baseline: Baseline value used in integration. Defaults to 0.
       x_steps: Number of integrated steps between baseline and x.
+      batch_size: Maximum number of x inputs (steps along the integration path)
+        that are passed to call_model_function as a batch.
     """
     if x_baseline is None:
       x_baseline = np.zeros_like(x_value)
@@ -38,12 +60,24 @@ class IntegratedGradients(GradientSaliency):
 
     x_diff = x_value - x_baseline
 
-    total_gradients = np.zeros_like(x_value)
+    total_gradients = np.zeros_like(x_value, dtype=np.float32)
 
+    x_step_batched = []
     for alpha in np.linspace(0, 1, x_steps):
       x_step = x_baseline + alpha * x_diff
+      x_step_batched.append(x_step)
+      if len(x_step_batched) == batch_size or alpha == 1:
+        x_step_batched = np.array(x_step_batched)
+        call_model_data = call_model_function(
+            x_step_batched,
+            call_model_args=call_model_args,
+            expected_keys=[INPUT_OUTPUT_GRADIENTS])
 
-      total_gradients += super(IntegratedGradients, self).GetMask(
-          x_step, feed_dict)
+        self.format_call_model_data(call_model_data,
+                                    x_step_batched.shape,
+                                    [INPUT_OUTPUT_GRADIENTS])
+
+        total_gradients += call_model_data[INPUT_OUTPUT_GRADIENTS].sum(axis=0)
+        x_step_batched = []
 
     return total_gradients * x_diff / x_steps
