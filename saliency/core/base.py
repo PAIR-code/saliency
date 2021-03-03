9c64b28285821be8cb342d84efcd7cea2d8c381a
Ben Wedin <ben.wedin@gmail.com>
2021-03-03 14:54:46 -0500

    Add edited versions of core files

--- saliency/core/base.py
+++ saliency/core/base.py
@@ -1,10 +1,10 @@
-# Copyright 2017 Google Inc. All Rights Reserved.
+# Copyright 2021 Google Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#    http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,51 +14,96 @@
 
 """Utilities to compute SaliencyMasks."""
 import numpy as np
-import tensorflow.compat.v1 as tf
 
-class SaliencyMask(object):
-  """Base class for saliency masks. Alone, this class doesn't do anything."""
-  def __init__(self, graph, session, y, x):
-    """Constructs a SaliencyMask by computing dy/dx.
 
-    Args:
-      graph: The TensorFlow graph to evaluate masks on.
-      session: The current TensorFlow session.
-      y: The output tensor to compute the SaliencyMask against. This tensor
-          should be of size 1.
-      x: The input tensor to compute the SaliencyMask against. The outer
-          dimension should be the batch size.
-    """
+# Output of the last convolution layer for the given input, including the batch
+# dimension.
+CONVOLUTION_LAYER_VALUES = 'CONVOLUTION_LAYER_VALUES'
+# Gradients of the output being explained (the logit/softmax value) with respect
+# to the last convolution layer, including the batch dimension.
+CONVOLUTION_OUTPUT_GRADIENTS = 'CONVOLUTION_OUTPUT_GRADIENTS'
+# Gradients of the output being explained (the logit/softmax value) with respect
+# to the input. Shape should be the same shape as x_value_batch.
+INPUT_OUTPUT_GRADIENTS = 'INPUT_OUTPUT_GRADIENTS'
+# Value of the output being explained (the logit/softmax value).
+OUTPUT_LAYER_VALUES = 'OUTPUT_LAYER_VALUES'
 
-    # y must be of size one, otherwise the gradient we get from tf.gradients
-    # will be summed over all ys.
-    size = 1
-    for shape in y.shape:
-      size *= shape
-    assert size == 1
+SHAPE_ERROR_MESSAGE = {
+    CONVOLUTION_LAYER_VALUES: (
+        'Expected outermost dimension of CONVOLUTION_LAYER_VALUES to be the '
+        'same as x_value_batch - expected {}, actual {}'
+    ),
+    CONVOLUTION_OUTPUT_GRADIENTS: (
+        'Expected outermost dimension of CONVOLUTION_OUTPUT_GRADIENTS to be the '
+        'same as x_value_batch - expected {}, actual {}'
+    ),
+    INPUT_OUTPUT_GRADIENTS: (
+        'Expected key INPUT_OUTPUT_GRADIENTS to be the same shape as input '
+        'x_value_batch - expected {}, actual {}'
+    ),
+    OUTPUT_LAYER_VALUES: (
+        'Expected outermost dimension of OUTPUT_LAYER_VALUES to be the same as'
+        ' x_value_batch - expected {}, actual {}'
+    ),
+}
 
-    self.graph = graph
-    self.session = session
-    self.y = y
-    self.x = x
 
-  def GetMask(self, x_value, feed_dict={}):
+class CoreSaliency(object):
+  r"""Base class for saliency methods. Alone, this class doesn't do anything."""
+
+  def GetMask(self, x_value, call_model_function, call_model_args=None):
     """Returns an unsmoothed mask.
 
     Args:
-      x_value: Input value, not batched.
-      feed_dict: (Optional) feed dictionary to pass to the session.run call.
+      x_value: Input ndarray.
+      call_model_function: A function that interfaces with a model to return
+        specific data in a dictionary when given an input and other arguments.
+        Expected function signature:
+        - call_model_function(x_value_batch,
+                              call_model_args={},
+                              expected_keys=None):
+          x_value_batch - Input for the model, given as a batch (i.e. dimension
+            0 is the batch dimension, dimensions 1 through n represent a single
+            input).
+          call_model_args - Other arguments used to call and run the model.
+          expected_keys - List of keys that are expected in the output. Possible
+            keys in this list are CONVOLUTION_LAYER_VALUES, 
+            CONVOLUTION_OUTPUT_GRADIENTS, INPUT_OUTPUT_GRADIENTS, and
+            OUTPUT_LAYER_VALUES, and are explained in detail where declared.
+      call_model_args: The arguments that will be passed to the call model
+        function, for every call of the model.
+
     """
     raise NotImplementedError('A derived class should implemented GetMask()')
 
-  def GetSmoothedMask(
-      self, x_value, feed_dict={}, stdev_spread=.15, nsamples=25,
-      magnitude=True, **kwargs):
+  def GetSmoothedMask(self,
+                      x_value,
+                      call_model_function,
+                      call_model_args=None,
+                      stdev_spread=.15,
+                      nsamples=25,
+                      magnitude=True,
+                      **kwargs):
     """Returns a mask that is smoothed with the SmoothGrad method.
 
     Args:
-      x_value: Input value, not batched.
-      feed_dict: (Optional) feed dictionary to pass to the session.run call.
+      x_value: Input ndarray.
+      call_model_function: A function that interfaces with a model to return
+        specific data in a dictionary when given an input and other arguments.
+        Expected function signature:
+        - call_model_function(x_value_batch,
+                              call_model_args={},
+                              expected_keys=None):
+          x_value_batch - Input for the model, given as a batch (i.e. dimension
+            0 is the batch dimension, dimensions 1 through n represent a single
+            input).
+          call_model_args - Other arguments used to call and run the model.
+          expected_keys - List of keys that are expected in the output. Possible
+            keys in this list are CONVOLUTION_LAYER_VALUES,
+            CONVOLUTION_OUTPUT_GRADIENTS, INPUT_OUTPUT_GRADIENTS, and
+            OUTPUT_LAYER_VALUES, and are explained in detail where declared.
+      call_model_args: The arguments that will be passed to the call model
+        function, for every call of the model.
       stdev_spread: Amount of noise to add to the input, as fraction of the
                     total spread (x_max - x_min). Defaults to 15%.
       nsamples: Number of samples to average across to get the smooth gradient.
@@ -68,10 +113,11 @@ class SaliencyMask(object):
     stdev = stdev_spread * (np.max(x_value) - np.min(x_value))
 
     total_gradients = np.zeros_like(x_value)
-    for i in range(nsamples):
+    for _ in range(nsamples):
       noise = np.random.normal(0, stdev, x_value.shape)
       x_plus_noise = x_value + noise
-      grad = self.GetMask(x_plus_noise, feed_dict, **kwargs)
+      grad = self.GetMask(x_plus_noise, call_model_function, call_model_args,
+                          **kwargs)
       if magnitude:
         total_gradients += (grad * grad)
       else:
@@ -79,19 +125,17 @@ class SaliencyMask(object):
 
     return total_gradients / nsamples
 
-class GradientSaliency(SaliencyMask):
-  r"""A SaliencyMask class that computes saliency masks with a gradient."""
-
-  def __init__(self, graph, session, y, x):
-    super(GradientSaliency, self).__init__(graph, session, y, x)
-    self.gradients_node = tf.gradients(y, x)[0]
-
-  def GetMask(self, x_value, feed_dict={}):
-    """Returns a vanilla gradient mask.
-
-    Args:
-      x_value: Input value, not batched.
-      feed_dict: (Optional) feed dictionary to pass to the session.run call.
-    """
-    feed_dict[self.x] = [x_value]
-    return self.session.run(self.gradients_node, feed_dict=feed_dict)[0]
+  def format_call_model_data(self, data, input_shape, expected_keys):
+    # For each expected_key in data, convert to numpy array and check shape against input_shape
+    use_outermost_only = [OUTPUT_LAYER_VALUES, CONVOLUTION_LAYER_VALUES,
+                          CONVOLUTION_OUTPUT_GRADIENTS]
+    for expected_key in expected_keys:
+      data[expected_key] = np.array(data[expected_key])
+      expected_shape = input_shape
+      actual_shape = data[expected_key].shape
+      if expected_key in use_outermost_only:
+        expected_shape = expected_shape[0]
+        actual_shape = actual_shape[0]
+      if expected_shape != actual_shape:
+        raise ValueError(SHAPE_ERROR_MESSAGE[expected_key].format(
+                       expected_shape, actual_shape))
