d6c82d797bc12acd979868d3ab65cc38990694d0
Ben Wedin <ben.wedin@gmail.com>
2021-03-03 17:24:48 -0500

    Add docstring to __init__

--- saliency/core/__init__.py
+++ saliency/core/__init__.py
@@ -1,4 +1,4 @@
-#Revert, with one line change
+"""Imports all classes from files so they can be called directly from saliency.core."""
 from .base import *
 from .blur_ig import *
 from .grad_cam import *
