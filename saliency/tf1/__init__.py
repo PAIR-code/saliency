502860269d84a527d940a69e662f21d13d39c103
Andrei Kapishnikov <>
2021-04-15 14:04:39 -0400

    Added implementation of the Guided IG attribution method.

--- saliency/tf1/__init__.py
+++ saliency/tf1/__init__.py
@@ -4,6 +4,7 @@ from .blur_ig import *
 from .grad_cam import *
 from .gradients import *
 from .guided_backprop import *
+from .guided_ig import *
 from .integrated_gradients import *
 from .occlusion import *
 from .utils import *
